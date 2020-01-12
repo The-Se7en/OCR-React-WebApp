d21fd552e43b02862b28f09e953795f426e15c43
kesavan <selvarajahkesavan@gmail.com>
2020-01-12 20:26:30 +0530

    added Signup functionality

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,51 +1,101 @@
 import React from "react";
-import '../StyleSheets/Signup.css';
+import "../StyleSheets/Signup.css";
+import Axios from "axios";
 
 class Signup extends React.Component {
+    state = {
+        username: "",
+        email: "",
+        password: ""
+    };
+
+    onSubmit = async e => {
+        e.preventDefault();
+        const { username, email, password } = this.state;
+        const body = {
+            username,
+            email,
+            password
+        };
+        console.log(body);
+        try {
+            const res = await Axios.post(
+                "http://localhost:8080/ocrUserApi/postOcr",
+                body,
+                { "Content-Type": "application/json" }
+            );
+            console.log(res);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
 
     render() {
-
         return (
             <div className="signup-form">
-                <form action="" method="post">
-
+                <form onSubmit={this.onSubmit}>
                     <h2>Sign Up</h2>
 
                     <p>Please fill this form to create an account</p>
 
-                    <hr/>
+                    <hr />
 
                     <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Username" required="required"/>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Username"
+                            required="required"
+                            onChange={e => this.setState({ username: e.target.value })}
+                        />
                     </div>
 
                     <div className="form-group">
-                        <input type="email" className="form-control" placeholder="Email" required="required"/>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Email"
+                            required="required"
+                            onChange={e => this.setState({ email: e.target.value })}
+                        />
                     </div>
 
                     <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password" required="required"/>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required="required"
+                            onChange={e => this.setState({ password: e.target.value })}
+                        />
                     </div>
+
+                    {/*<div className="form-group">*/}
+                    {/*    <input type="password" className="form-control" placeholder="Confirm Password" required="required"/>*/}
+                    {/*</div>*/}
 
                     <div className="form-group small clearfix">
-                        <label className="checkbox-inline"><input type="checkbox"/> I accept the <a href="#">Terms of
-                            Use</a> &amp; <a href="#">Privacy Policy</a></label>
+                        <label className="checkbox-inline">
+                            <input type="checkbox" /> I accept the{" "}
+                            <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a>
+                        </label>
                     </div>
 
-                    <input type="submit" className="btn btn-primary btn-block btn-lg" value="Signup"/>
+                    <input
+                        type="submit"
+                        className="btn btn-primary btn-block btn-lg"
+                        value="Signup"
+                    />
 
-                    <br/>
+                    <br />
 
                     <div className="text-center small">
                         Already have an account? <a href="/login">Login here</a>
                     </div>
-
                 </form>
             </div>
         );
-
     }
-
 }
 
 export default Signup;
