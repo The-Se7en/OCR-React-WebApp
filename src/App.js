dcdebc0cb2eafa90fb9d784c4e5f476bb8971690
kesavan <selvarajahkesavan@gmail.com>
2020-01-12 18:59:44 +0530

    Browser Router Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Header from "./components/Header";
-import Home from "./components/Home";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from "./Components/Header";
+import Welcome from "./Components/Welcome";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Home from "./Components/Home";
 
 function App() {
 
   return (
-    <div>
-      <Header></Header>
-      <Home></Home>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Header />
+              <Switch>
+                  <Route exact path='/'component={Welcome} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/signup' component={Signup} />
+                  <Route path='/home' component={Home} />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Welcome extends React.Component{
+
+    render() {
+        return(
+            <div>
+                <p>This is Welcome</p>
+            </div>
+        );
+    }
+
+}
+
+export default Welcome;
