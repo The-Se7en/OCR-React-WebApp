39a49e0739440928731269aa578c690c0754b81e
kesavan <selvarajahkesavan@gmail.com>
2020-01-12 19:45:47 +0530

    Navbar update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,32 @@
 import React from "react";
+import '../StyleSheets/Header.css';
+import logo from "../Images/logo.png";
 
-class Header extends React.Component{
+class Header extends React.Component {
 
     render() {
-        return(
-          <div>
-              <p>This is Header</p>
-          </div>
+
+        return (
+            <div>
+
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="/">
+                                <img src={logo} alt="logo"/>
+                            </a>
+                            <h2 className="navbar-brand">OMG OCR</h2>
+
+                        </div>
+                        <a className="nav-link" href="/signup">Signup</a>
+                        <a className="nav-link" href="/login">Login</a>
+                    </div>
+                </nav>
+                <br/>
+
+            </div>
         );
+
     }
 
 }
